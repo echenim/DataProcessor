4831f881b24dd2f8fa798394829e738dd27523e1
william echenim <myron.echenim@gmail.com>
2024-04-04 13:12:50 -0700

    more updates

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"context"
+
+	"github.com/echenim/data-processor/clients"
+	"github.com/echenim/data-processor/config"
+	"github.com/echenim/data-processor/logger"
+	"github.com/echenim/data-processor/processor"
+)
+
+func main() {
+	logger.Setup()
+	cfg, err := config.Load()
+	if err != nil {
+		logger.Error("Failed to load configuration:", err)
+		return
+	}
+
+	ctx := context.Background()
+	processor, err := processor.NewProcessor(ctx, cfg)
+	if err != nil {
+		logger.Error("Failed to initialize processor:", err)
+		return
+	}
+
+	clients.StartSubscriber(ctx, cfg, processor.ProcessMessage)
+}
